0e09b9eb33ab9fdf1dad97c2ce02310386c4024a
Jeremiah Hukpati <58642285+Jerry1811@users.noreply.github.com>
2022-03-17 12:14:32 +0000

    add Hero section template

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,48 @@
-import { Hero } from './HeroStyles'
+import Carousel from 'react-bootstrap/Carousel'
+
 const HeroSection = () => {
   return (
     <>
-      <Hero></Hero>
+      <Carousel fade style={{ marginTop: 'auto' }}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/200/300?random=1"
+            alt="First slide"
+            style={{ width: '100px', height: '500px' }}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/200/300?random=1"
+            alt="Second slide"
+            style={{ width: '100px', height: '500px' }}
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/200/300?random=1"
+            alt="Third slide"
+            style={{ width: '100px', height: '500px' }}
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </>
   )
 }
